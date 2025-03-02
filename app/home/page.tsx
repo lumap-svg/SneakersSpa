31d5359fd73097e3dada150dd730fe70344020c3
Lumap <lutanywapeter@gmail.com>
2025-03-02 08:25:43 +0300

    first commit of the project

---
+++ app/home/page.tsx
@@ -0,0 +1,25 @@
+import Display from '@/components/display'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <header className=' h-24 px-2 flex flex-grow justify-between items-end'>
+            <h2 className='uppercase  text-3xl font-semibold  '>sneaker spa</h2>
+            
+                
+            <ul className='flex gap-3  font-semibold uppercase '>
+                <li>orders</li>
+                <li>customers </li>
+                <button className='uppercase'>messages</button>
+                <li>payments</li>
+                
+            </ul>
+        </header>
+            <hr className='font-bold w-full'/>
+            <main>
+                <Display />
+            </main>
+        </div>
+  )
+}
