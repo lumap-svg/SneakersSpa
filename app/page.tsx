c3e07e8e7c45edcd58b6ddf6a502eefbe578726e
Lumap <lutanywapeter@gmail.com>
2025-03-03 23:42:35 +0300

    adjsuted the landing display

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,14 @@ export default function Home() {
   return (
     <div >
       {/* https://www.pinterest.com/pin/shoe-clean-service-landing-page--1054264594003232096/ */}
-    <header className="flex items-center gap-2 flex-grow">
+    <header className="flex items-center gap-2 flex-grow mb-1">
       
                   <Image className='rounded-full mt-3 bg-white' src={'/sneakers spa 254.jpeg'} alt={'company log'} width={200} height={200} />
                   <h2 className="text-4xl font-bold uppercase text-[#ebd455f8] ">Sneakers spa 254  </h2>
                  <button className="ml-auto uppercase font-semibold border-2 p-2 px-3 border-blue-500 rounded-full text-[#ebd455f8]">login</button>
       
     </header>
+    <hr className="w-full mx-1"/>
     <main className=" max-w-6xl mx-auto mt-4 ">
 
       {/* welcoming heros landing page */}
--- components/heroslanding.tsx
+++ components/heroslanding.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 export default function  Heroslanding() {
   return (
-    <div className='flex  my-3 bg-gray-200 gap-2 rounded-lg'>
+    <div className='flex  my-3 bg-gray-200 gap-2 rounded-lg overflow-hidden '>
         {/* image */}
         <Image className='rounded-r-full ' src={'/daniel-storek-JM-qKEd1GMI-unsplash.jpg'}  width={500} height={400} alt='shoe game' />
                 {/* message */}
                 <div className=' flex flex-col justify-around'>
                     <h1 className='text-5xl font-bold text-left'>Give Your Sneakers the Spa Treatment They Deserve!</h1>
-                    <h3 className='text-3xl font-semibold text-[#fbbe30]'>Machine-washed, dried, and ready in just 2 hours.</h3>
-                    <button className='rounded-full bg-blue-600 w-[200]  p-3 '>Get Started</button>
+                    <h3 className='text-3xl font-semibold '>Machine-washed, dried, and ready in just 2 hours.</h3>
+                    <button className='rounded-full border border-[#52480ff8]  w-[200]  p-3 '>Get Started {">"}</button>
                 </div>
     </div>
   )
