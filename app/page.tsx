203bb0e1d02eeced1c6c0e572f9bf085f176664a
Lumap <lutanywapeter@gmail.com>
2025-03-03 23:31:55 +0300

    adjusted the color

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <header className="flex items-center gap-2 flex-grow">
       
                   <Image className='rounded-full mt-3 bg-white' src={'/sneakers spa 254.jpeg'} alt={'company log'} width={200} height={200} />
-                  <h2 className="text-4xl font-bold uppercase">Sneakers spa 254  </h2>
-                 <button className="ml-auto uppercase font-semibold border-2 p-2 px-3 border-blue-500 rounded-full text-white">login</button>
+                  <h2 className="text-4xl font-bold uppercase text-[#ebd455f8] ">Sneakers spa 254  </h2>
+                 <button className="ml-auto uppercase font-semibold border-2 p-2 px-3 border-blue-500 rounded-full text-[#ebd455f8]">login</button>
       
     </header>
     <main className=" max-w-6xl mx-auto mt-4 ">
